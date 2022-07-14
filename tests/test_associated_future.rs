fbe9f8ecc543f392eb126c3ce739e64552380a19
Audun Halland <audun.halland@pm.me>
2022-07-14 17:07:09 +0200

    test: organize nightly tests, add one more

--- tests/test_associated_future.rs
+++ tests/test_associated_future.rs
@@ -4,48 +4,74 @@
 
 use unimock::*;
 
-#[unimock]
-trait WithoutUnmock {
-    type Fut<'a>: ::core::future::Future<Output = i32> + Send
-    where
-        Self: 'a;
+mod without_unmock {
+    use super::*;
 
-    fn without_unmock<'a>(&'a self, arg: i32) -> Self::Fut<'a>;
+    #[unimock]
+    trait WithoutUnmock {
+        type Fut<'a>: ::core::future::Future<Output = i32> + Send
+        where
+            Self: 'a;
+
+        fn without_unmock<'a>(&'a self, arg: i32) -> Self::Fut<'a>;
+    }
+
+    #[tokio::test]
+    async fn should_mock_without_unmock() {
+        let unimock = mock([WithoutUnmock__without_unmock
+            .each_call(matching!(10))
+            .returns(20)
+            .in_any_order()]);
+
+        let answer = unimock.without_unmock(10).await;
+
+        assert_eq!(20, answer);
+    }
 }
 
-#[unimock(unmocked=[do_unmock(arg)])]
-trait WithUnmock {
-    type Fut<'a>: ::core::future::Future<Output = i32> + Send
-    where
-        Self: 'a;
+mod with_unmock {
+    use super::*;
 
-    fn with_unmock<'a>(&'a self, arg: i32) -> Self::Fut<'a>;
+    #[unimock(unmocked=[do_unmock(arg)])]
+    trait WithUnmock {
+        type Fut<'a>: ::core::future::Future<Output = i32> + Send
+        where
+            Self: 'a;
+
+        fn with_unmock<'a>(&'a self, arg: i32) -> Self::Fut<'a>;
+    }
+
+    async fn do_unmock(arg: i32) -> i32 {
+        arg * 2
+    }
+
+    #[tokio::test]
+    async fn should_mock_with_unmock() {
+        let unimock = mock([WithUnmock__with_unmock
+            .each_call(matching!(42))
+            .unmocked()
+            .in_any_order()]);
+
+        let answer = unimock.with_unmock(42).await;
+
+        assert_eq!(84, answer);
+    }
 }
 
-async fn do_unmock(arg: i32) -> i32 {
-    arg * 2
-}
+mod reference_argument_works_with_explicit_lifetime {
+    use super::*;
 
-#[tokio::test]
-async fn should_mock_without_unmock() {
-    let unimock = mock([WithoutUnmock__without_unmock
-        .each_call(matching!(10))
-        .returns(20)
-        .in_any_order()]);
+    struct Error;
 
-    let answer = unimock.without_unmock(10).await;
-
-    assert_eq!(20, answer);
-}
-
-#[tokio::test]
-async fn should_mock_with_unmock() {
-    let unimock = mock([WithUnmock__with_unmock
-        .each_call(matching!(42))
-        .unmocked()
-        .in_any_order()]);
-
-    let answer = unimock.with_unmock(42).await;
-
-    assert_eq!(84, answer);
+    #[unimock(debug = true)]
+    trait GetUsername {
+        type Fut<'entrait, 'i1>: ::core::future::Future<Output = Result<String, Error>> + Send
+        where
+            Self: 'entrait;
+        fn get_username<'entrait, 'i1>(
+            &'entrait self,
+            id: u32,
+            password: &'i1 str,
+        ) -> Self::Fut<'entrait, 'i1>;
+    }
 }
