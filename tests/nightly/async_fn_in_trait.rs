c6df09bc8208e1e56a0dacb63942141657774aab
Audun Halland <audun.halland@pm.me>
2022-11-20 15:53:48 +0100

    test: Add a couple of things to async_fn_in_trait test

--- tests/nightly/async_fn_in_trait.rs
+++ tests/nightly/async_fn_in_trait.rs
@@ -3,11 +3,19 @@ use unimock::*;
 #[unimock(api = TraitMock)]
 trait Trait {
     async fn a(&self, arg: i32) -> i32;
+    async fn b(&self) -> &i32;
+    async fn c(&self) -> Option<&i32>;
 }
 
 #[tokio::test]
 async fn test_it() {
-    let deps = Unimock::new(TraitMock::a.next_call(matching!(_)).returns(42));
+    let deps = Unimock::new((
+        TraitMock::a.next_call(matching!(_)).returns(42),
+        TraitMock::b.next_call(matching!()).returns(42),
+        TraitMock::c.next_call(matching!()).returns(Some(42)),
+    ));
 
     assert_eq!(42, deps.a(5).await);
+    assert_eq!(&42, deps.b().await);
+    assert_eq!(Some(&42), deps.c().await);
 }
