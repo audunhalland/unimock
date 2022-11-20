2e14f85c6ff1fdcf78891917f7642703183b42ab
Audun Halland <audun.halland@pm.me>
2022-11-20 17:54:08 +0100

    test: Combine an eq test and a regular guard in matching_eq

--- tests/it/matching_eq.rs
+++ tests/it/matching_eq.rs
@@ -21,7 +21,7 @@ fn test_eq() {
             .each_call(matching!(_, eq!(&Data(vec![3]))))
             .returns(3),
         TestMock::f
-            .each_call(matching!(_, ne!(&Data(vec![1337]))))
+            .each_call(matching!((a, ne!(&Data(vec![1337]))) if a != &0 ))
             .returns(42),
         TestMock::f.each_call(matching!(_, _)).returns(1337),
     ));
@@ -30,6 +30,7 @@ fn test_eq() {
     assert_eq!(2, <Unimock as Test>::f(&u, 0, Data(vec![2])));
     assert_eq!(3, <Unimock as Test>::f(&u, 0, Data(vec![3])));
     assert_eq!(1, <Unimock as Test>::f(&u, 0, Data(vec![1])));
-    assert_eq!(42, <Unimock as Test>::f(&u, 0, Data(vec![42])));
+    assert_eq!(1337, <Unimock as Test>::f(&u, 0, Data(vec![42])));
+    assert_eq!(42, <Unimock as Test>::f(&u, 1, Data(vec![42])));
     assert_eq!(1337, <Unimock as Test>::f(&u, 0, Data(vec![1337])));
 }
