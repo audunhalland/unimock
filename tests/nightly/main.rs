6cf82fde0d9ab94eef510b2712e571d3188ed091
Audun Halland <audun.halland@pm.me>
2023-04-16 04:49:38 +0200

    test(nightly): Add feature(impl_trait_in_assoc_type)

--- tests/nightly/main.rs
+++ tests/nightly/main.rs
@@ -3,6 +3,7 @@
 #![feature(type_alias_impl_trait)]
 #![feature(async_fn_in_trait)]
 #![feature(closure_track_caller)]
+#![feature(impl_trait_in_assoc_type)]
 
 mod associated_future;
 mod async_fn_in_trait;
