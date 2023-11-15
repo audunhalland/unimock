d1ac166354f63d96bf57194a0a4c2ebc902acd17
Audun Halland <audun.halland@pm.me>
2023-11-15 14:51:11 +0100

    chore: Update test_all_stable.sh script

--- test_all_stable.sh
+++ test_all_stable.sh
@@ -4,5 +4,5 @@ set -x
 
 (cd unimock_macros; cargo test)
 cargo hack --feature-powerset --features std --exclude-features default,spin-lock,nightly-tests,unstable-doc-cfg test
-cargo hack --feature-powerset --exclude-features std,mock-std,default,nightly-tests,unstable-doc-cfg test
+cargo hack --feature-powerset --features critical-section --exclude-features std,mock-std,default,nightly-tests,unstable-doc-cfg test
 cargo test --doc --features mock-core,mock-std
